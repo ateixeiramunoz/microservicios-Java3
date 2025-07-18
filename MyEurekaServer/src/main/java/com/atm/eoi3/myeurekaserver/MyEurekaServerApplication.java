f420be19d81efe40cf3dafeba6701a7bc94e16dc
Alejandro Teixeira Muño <mail.alejandro.teixeira@gmail.com>
2025-07-18 12:08:52 +0200

    asdad

--- MyEurekaServer/src/main/java/com/atm/eoi3/myeurekaserver/MyEurekaServerApplication.java
+++ MyEurekaServer/src/main/java/com/atm/eoi3/myeurekaserver/MyEurekaServerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-
 public class MyEurekaServerApplication {
 
     public static void main(String[] args) {
